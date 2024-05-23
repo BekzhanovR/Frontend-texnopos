f7c561da0dea6bed13af9c4f37fd7670d466ca71
BekzahnovR <bekzhanovofficial@gmail.com>
2024-05-23 18:09:39 +0500

    Header updated

--- Project-03 (DoctorCare)/assets/app.js
+++ Project-03 (DoctorCare)/assets/app.js
@@ -1,10 +1,13 @@
 // Variables
 const header = document.querySelector("header")
+const logo = document.querySelector("#logo")
 
 window.addEventListener("scroll", () => {
     if(window.scrollY > 0){
         header.classList.add("active")
+        logo.src = 'assets/img/secondary-logo.svg'
     }else{
         header.classList.remove("active")
+        logo.src = 'assets/img/primary-logo.svg'
     }
 })  

87e30aec5556ae1231f251d05a2ee583506efcbb
BekzahnovR <bekzhanovofficial@gmail.com>
2024-05-25 13:45:29 +0500

    Update

--- Project-03 (DoctorCare)/assets/app.js
+++ Project-03 (DoctorCare)/assets/app.js
@@ -3,6 +3,7 @@ const header = document.querySelector("header")
 const logo = document.querySelector("#logo")
 const navLinks = document.querySelectorAll("nav li a")
 const sections = document.querySelectorAll(".section")
+const hambBtn = document.querySelector(".hamb")
 
 window.addEventListener("scroll", () => {
     if(window.scrollY > 0){
@@ -26,4 +27,11 @@ window.addEventListener("scroll", () => {
             })          
         }
     })
+})
+
+// A function that add a class when the button is clicked
+hambBtn.addEventListener('click', () => {
+    hambBtn.classList.toggle('active')
+    header.classList.toggle('active')
+    
 })

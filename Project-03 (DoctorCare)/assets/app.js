d61c4cde7505083198012714d04224dd5c7c8dd5
BekzahnovR <bekzhanovofficial@gmail.com>
2024-05-25 10:37:28 +0500

    Update nav bar on scroll

--- Project-03 (DoctorCare)/assets/app.js
+++ Project-03 (DoctorCare)/assets/app.js
@@ -1,7 +1,8 @@
 // Variables
 const header = document.querySelector("header")
 const logo = document.querySelector("#logo")
-const navLinks = document.querySelectorAll("nav li")
+const navLinks = document.querySelectorAll("nav li a")
+const sections = document.querySelectorAll(".section")
 
 window.addEventListener("scroll", () => {
     if(window.scrollY > 0){
@@ -11,8 +12,23 @@ window.addEventListener("scroll", () => {
         header.classList.remove("active")
         logo.src = 'assets/img/primary-logo.svg'
     }
+
+    sections.forEach(section => {
+        const top = window.scrollY + 150
+        const offset = section.offsetTop
+        const height = section.offsetHeight
+        const id = section.getAttribute('id')
+
+        if(top >= offset && top < offset + height){
+            navLinks.forEach(navLink => {
+                navLink.classList.remove("active");
+                document.querySelector('nav a[href*=' + id + ']').classList.add('active')
+            })          
+        }
+    })
 })  
 
+// Update nav links on click
 navLinks.forEach(element => {
     element.addEventListener("click", () => {
         navLinks.forEach(navLink => navLink.classList.remove("active"))
